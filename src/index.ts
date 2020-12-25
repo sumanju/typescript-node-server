86d2221167608a2fc98fd747a36b9e6319e459f9
sumanmandal <suman.mandal@obopay.com>
2020-12-25 18:35:07 +0530

    ts to js compilation

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,13 @@
-import express from 'express'
-import path from 'path'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-const routeList     = require('./route-list')
-// import socket from 'socket.io'
-// const conn          = require('./src/db-connection/mysql-db')
+import express      from 'express'
 
 const app = express()
-      app.use(bodyParser.json())
-      app.use(cors())
-      app.use(routeList)
-      app.use(cookieParser())
 
-      app.use(function (req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*")
-        res.header(
-          "Access-Control-Allow-Headers",
-          "Origin, X-Requested-With, Content-Type, Accept"
-        )
-        next()
-      })
+app.get("/", (req, res) => {
+  res.send("hello")
+})
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5500
 
 const server = app.listen(port, () => {
   console.log('running ', port)
 })
-
-// const io   = socket(server)
-
-// io.sockets.on('connection', (socket) => {
-//   socket.on('mesg', (data) => {
-//     socket.emit('event', {})
-//   })
-// })
